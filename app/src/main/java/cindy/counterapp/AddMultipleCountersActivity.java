f7c8c94e98371f6ebc7de6f9bf9b5f3771f5166b
Jeremy Mowery <jermowery@email.arizona.edu>
2016-08-12 11:09:03 -0700

    Add single and multiple counters activities

---
+++ app/src/main/java/cindy/counterapp/AddMultipleCountersActivity.java
@@ -0,0 +1,13 @@
+package cindy.counterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMultipleCountersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_multiple_counters);
+    }
+}
--- app/src/main/java/cindy/counterapp/AddSingleCounterActivity.java
+++ app/src/main/java/cindy/counterapp/AddSingleCounterActivity.java
@@ -3,7 +3,7 @@ package cindy.counterapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddCounterActivity extends AppCompatActivity {
+public class AddSingleCounterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
